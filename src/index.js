66ff739ea212b55e2444cda724b1c665599d4edf
Olawale <olapetanwale@outlook.com>
2022-06-14 22:10:25 +0100

    Something to test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
+
 const message = document.querySelector('.para');
-console.log(message)
-message.addEventListener('click', ()=>{
-    document.body.classList.toggle('bg-change')
-})
-
-
+message.addEventListener('click', () => {
+  document.body.classList.toggle('bg-change');
+});
