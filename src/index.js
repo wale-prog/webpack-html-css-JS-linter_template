6b6d43b4b324b6a2f903c28e8db69d11391826fb
Olawale <olapetanwale@outlook.com>
2022-06-14 22:04:18 +0100

    Final setup for linters and webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './style.css';
+const message = document.querySelector('.para');
+console.log(message)
+message.addEventListener('click', ()=>{
+    document.body.classList.toggle('bg-change')
+})
 
-const add = (a, b) => a + b;
-add();
+
